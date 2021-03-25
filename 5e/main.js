53c6c644bebebecd65047be1d610088a971d772c
Cyriaque <cyriaque.couenon+kraken@gmail.com>
2021-03-25 04:31:28 +0100

    added disadv for 5e tool

--- 5e/main.js
+++ 5e/main.js
@@ -13,7 +13,7 @@ function hit_chance(bonus) {
     return Math.max(0.05, Math.min(0.95, 0.05 * bonus));
 }
 
-function get_custom_thresh(hit, dmg, deg) {
+function get_supadv_thresh(hit, dmg, deg) {
     let basic_dmg = 0;
     let gwm_dmg = 0;
     let ac = 15 + hit; // problem with starting at 20 + hit : GWM always comes up on top when only a crit hits
@@ -26,21 +26,37 @@ function get_custom_thresh(hit, dmg, deg) {
     return ac;
 }
 
+// TODO : move that to supadv to have a generic function
+function get_disadv_thresh(hit, dmg) {
+    let basic_dmg = 0;
+    let gwm_dmg = 0;
+    let ac = 15 + hit;
+    for (; ac > 0 ; ac--) {
+        basic_dmg = Math.pow(1 - hit_chance(ac - hit - 1), 2) * dmg;
+        gwm_dmg = Math.pow(1 - hit_chance(ac - hit + 4), 2) * (dmg + 10);
+        if (gwm_dmg > basic_dmg)
+            break;
+    }
+    return ac;
+}
+
 // maths pulled from https://forums.giantitp.com/showthread.php?472938-Great-Weapon-Mastery-How-to-5-10-Like-a-Pro
 function get_thresh(hit, dmg, adv) {
     hit = parseFloat(hit);
     dmg = get_avg(dmg);
     if (isNaN(hit) || isNaN(dmg) || dmg === 0)
         return 0;
+    if (adv === 3)
+    return get_disadv_thresh(hit, dmg);
     if (adv === 2)
-        return get_custom_thresh(hit, dmg, 3);
+        return get_supadv_thresh(hit, dmg, 3);
     if (adv === 1)
         return min_thresh(0.5 * (2 * hit + Math.sqrt((Math.pow(dmg, 2) + 10 * dmg + 1600)) - dmg - 8));
     return min_thresh(hit - dmg / 2 + 16);
 }
 
 function compute() {
-    let adv = $("#adv2").is(":checked") ? 2 : ($("#adv1").is(":checked") ? 1 : 0);
+    let adv = $("#adv3").is(":checked") ? 3 : $("#adv2").is(":checked") ? 2 : ($("#adv1").is(":checked") ? 1 : 0);
     let hit = $("#hit_bonus").val();
     let dmg = $("#damage").val();
     $("#res-field").text(get_thresh(hit, dmg, adv));
